131f46a7b94d177eebda5ad5d26aaecd43356b6b
1000ch <shogo.sensui@gmail.com>
2019-08-19 02:01:06 +0900

    Add tests

---
+++ test/x-cell.test.ts
@@ -0,0 +1,7 @@
+import XCell from '../src/x-cell';
+
+describe('XCell', (): void => {
+  test('should be defined', (): void => {
+    expect(XCell).toBeDefined();
+  });
+})
---
+++ test/x-grid.test.ts
@@ -0,0 +1,7 @@
+import XGrid from '../src/x-grid';
+
+describe('XGrid', (): void => {
+  test('should be defined', (): void => {
+    expect(XGrid).toBeDefined();
+  });
+})
