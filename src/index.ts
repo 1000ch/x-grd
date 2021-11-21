7e340d4e86e4de3e6ebd00a9bd92d39d85563231
1000ch <shogosensui@gmail.com>
2021-11-21 13:05:38 +0900

    feat: add tagName declaration

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ import XGrid from './x-grid.js';
 import XCell from './x-cell.js';
 
 export { XGrid, XCell };
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'x-grid': XGrid;
+    'x-cell': XCell;
+  }
+}
