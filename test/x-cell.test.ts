b7f87461dbb83c0923ba90535816fab735c7b501
1000ch <shogosensui@gmail.com>
2021-11-22 17:46:47 +0900

    test: add tests

--- test/x-cell.test.ts
+++ test/x-cell.test.ts
@@ -1,7 +1,15 @@
 import XCell from '../src/x-cell';
 
 describe('XCell', (): void => {
+  beforeAll(() => {
+    customElements.define('x-cell', XCell);
+  });
+
   test('should be defined', (): void => {
     expect(XCell).toBeDefined();
+    expect(customElements.get('x-cell')).toBe(XCell);
+
+    const cell = new XCell();
+    expect(cell).toBeInstanceOf(HTMLElement);
   });
 })
--- test/x-grid.test.ts
+++ test/x-grid.test.ts
@@ -1,7 +1,15 @@
 import XGrid from '../src/x-grid';
 
 describe('XGrid', (): void => {
+  beforeAll(() => {
+    customElements.define('x-grid', XGrid);
+  });
+
   test('should be defined', (): void => {
     expect(XGrid).toBeDefined();
+    expect(customElements.get('x-grid')).toBe(XGrid);
+
+    const grid = new XGrid();
+    expect(grid).toBeInstanceOf(HTMLElement);
   });
 })
